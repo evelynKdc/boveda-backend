45f65a079a05e2c4cba5e78b60fcd98c74597061
evelynKdc <evelyndc_10@hotmail.com>
2025-11-02 00:35:25 -0500

    feat(auth): add GET /auth/profile endpoint
    Adds the ability to fetch the currently authenticated user's profile using their JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
 // src/auth/auth.controller.ts
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, UseGuards, Get, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { ChallengeDto } from './dto/challenge.dto';
 import { VerifyDto } from './dto/verify.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private usersService: UsersService,) { }
 
   @Post('register')
   register(@Body() registerDto: RegisterDto) {
@@ -24,5 +26,13 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   verify(@Body() verifyDto: VerifyDto) {
     return this.authService.verify(verifyDto);
+  }
+
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async getProfile(@Request() req) {
+    const userId = req.user.userId;
+    return this.usersService.findById(userId);
   }
 }
